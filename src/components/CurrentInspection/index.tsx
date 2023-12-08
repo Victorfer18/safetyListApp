c3816ee73698d34de1631a7991a27562dcdc8edf
victor fernando <victorfernandomagalhaes@gmail.com>
2023-12-08 19:32:44 -0300

    fix

---
+++ src/components/CurrentInspection/index.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Text } from "react-native";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const styleHeaderInspectionName = StyleSheet.create({
+    box: {
+        backgroundColor: "rgba(0, 0, 0, 0.3)",
+        padding: 16,
+
+    },
+    text: {
+        color: "#ccc",
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 20,
+    }
+})
+
+export function setInspectionName(name: string) {
+    AsyncStorage.removeItem('CurrentInspection')
+        .then(() => {
+            AsyncStorage.setItem('CurrentInspection', name);
+        });
+}
+
+
+export default function () {
+
+    const [name, setName] = useState('')
+
+    useEffect(() => {
+        (async () => {
+            let InspectionName = await AsyncStorage.getItem('CurrentInspection') || '';
+            setName(InspectionName)
+        })()
+    }, []);
+
+    return (
+        <View style={styleHeaderInspectionName.box}>
+            <Text style={styleHeaderInspectionName.text}>{name}</Text>
+        </View>
+    )
+}
